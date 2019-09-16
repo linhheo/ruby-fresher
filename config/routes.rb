01bb7a19d47d98bfb2dceffbea160d2178e4f56a
Heo <tlinhhpu2@gmail.com>
2019-09-16 11:26:05 +0700

    dang sang login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
   resources :microposts
   resources :users
+  get  '/signup',  to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
